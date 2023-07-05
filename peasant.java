031760f41baeb7260ebbb5791a72bdcd08776bd3
AlexeyMonkey13 <Aptuct89@inbox.ru>
2023-07-06 00:07:48 +0700

    добавил героев и реализовал методы

---
+++ peasant.java
@@ -0,0 +1,7 @@
+public class peasant extends Heros{
+    public peasant(String loli) {
+        super(6, 40, loli, "peasant", 0,2);
+    }
+}
+
+
---
+++ superman.java
@@ -0,0 +1,5 @@
+public class superman extends closery_fight{
+    public superman(String name) {
+        super(3, 110, name, "superman", 10, 4);
+    }
+}
